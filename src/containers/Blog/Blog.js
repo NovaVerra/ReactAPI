f500e4bff21b0d270f01cf80bedb83bd5fe8a911
lununglee <leo.lunung.lee@gmail.com>
2019-10-07 19:51:39 -0700

    R12 - Creating and Using Axios Instance

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import Axios from 'axios'
+// import Axios from 'axios'
+import Axios from '../../axios'
 
 import Post from '../../components/Post/Post'
 import FullPost from '../../components/FullPost/FullPost'
